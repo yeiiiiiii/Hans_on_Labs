b8ea59e53a046318d778726d0f30e66f134360e7
bella <bellotareales@gmail.com>
2024-11-03 23:20:03 -0500

    Menu acabado, con configuraciones visuales

--- Hands_on_Labs/main.py
+++ Hands_on_Labs/main.py
@@ -3,15 +3,15 @@ from modulos.registros_lista import*
 def menu():
 
     while True:
-        print(" . . . . . . . . . 𝗠𝗲𝗻𝘂 𝗣𝗿𝗶𝗻𝗰𝗶𝗽𝗮𝗹 . . . . . . . . . ")
-        print(" . 𝟏. Registrar vehículo en exhibición            . ")
-        print(" . 𝟐. Registrar venta diaria                      . ")
-        print(" . 𝟑. Lista de vehículos en exhibición            . ")
-        print(" . 𝟒. Reporte de ventas por mes                   . ")
-        print(" . 𝟓. Salir                                       . ")
-        print(" . . . . . . . . . . . . . . . . . . . . . . . . .  ")
-        print(" . Seleccione una opción")
-        opcion=input("--> ")
+        print(" . . . . . . . . . 𝗠𝗲𝗻𝘂 𝗣𝗿𝗶𝗻𝗰𝗶𝗽𝗮𝗹 . . . . . . . . . . ")
+        print(" . 𝟏. Registrar v͟e͟h͟i͟c͟u͟l͟o͟ en exhibición ( 🚘 )      . ")
+        print(" . 𝟐. Registrar venta d͟i͟a͟r͟i͟a͟ ( 💸 )                . ")
+        print(" . 𝟑. l͟i͟s͟t͟a͟ de vehículos en exhibición ( 📜 )      . ")
+        print(" . 𝟒. Reporte de ventas por m͟e͟s͟ ( 📊 )             . ")
+        print(" . 𝟓. Salir ( 🚫 )                                 . ")
+        print(" . . . . . . . . . . . . . . . . . . . . . . . . . .  ")
+        print(" . S͟e͟l͟e͟c͟c͟i͟o͟n͟e͟ u͟n͟a͟ o͟p͟c͟i͟o͟n͟ ! !")
+        opcion=input(" ❓ = ")
         
         if opcion == '1':
             registrar_vehiculo()
@@ -22,7 +22,7 @@ def menu():
         elif opcion == '4':
             generar_reporte_ventas()
         elif opcion == '5':
-            print("Saliendo del programa...")
+            print("🚪 . . . Saliendo del programa ")
             break
         else:
             print("Opción no permitida, es del 1 al 5")
